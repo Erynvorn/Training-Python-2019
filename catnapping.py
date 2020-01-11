d3eb94f0caf9e65715b96bd15139b4a130135764
Erynvorn <daniel.seite@gmail.com>
2020-01-11 11:14:21 -0500

    cat

---
+++ catnapping.py
@@ -0,0 +1,7 @@
+print('''Dear Eve,
+
+Dan's cat has been eating a lot.
+
+Sincerely,
+Bob''')
+print('Dear Alice,\n\nEve\'s cat has been arrested for catnapping, cat burglary, and extortion.\n\nSincerely,\nBob')
