cf60c08cc27a4d31fb590044d471b824243e8abc
Erynvorn <daniel.seite@gmail.com>
2020-01-26 10:16:09 -0500

    three

---
+++ bulletPointAdder.py
@@ -0,0 +1,15 @@
+#! /usr/bin/env python3
+# bulletPointAdder.py
+# of each line of the text on the clipboard.
+
+import pyperclip
+text = pyperclip.paste()
+
+# Separate lines and add stars.
+
+lines = text.split('\n')
+for i in range(len(lines)):   # loop through all indexes in the "lines" list
+    lines[i] = '* '+ lines[i] # add star to each string in "lines" list
+text = '\n'.join(lines)
+    
+pyperclip.copy(text)
---
+++ hellofunc2.py
@@ -0,0 +1,8 @@
+def hello(name):
+    print('Howdy! '+ name)
+    print('Howdy!!! '+ name)
+    print('Hello there.')
+
+hello('Alice')
+hello('Bob')
+hello('dan')
