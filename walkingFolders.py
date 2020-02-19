ba0db148d0601cac3fffc489e93b83764c64d7ed
Erynvorn <daniel.seite@gmail.com>
2020-02-19 00:08:00 -0500

    three

---
+++ walkingFolders.py
@@ -0,0 +1,9 @@
+import os
+
+for folderName, subfolders, filenames in os.walk('.'):
+    print('The current folder is ' + folderName)
+
+    for subfolder in subfolders:
+        print('Subfolder of ' + folderName + ': ' + subfolder)
+    for filename in filenames:
+        print('File inside ' + folderName + ': ' + filename)
