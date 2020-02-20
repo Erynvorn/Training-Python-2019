9a4d7e50180a3f1dbf7022d2364b275a8d35b640
Erynvorn <daniel.seite@gmail.com>
2020-02-20 07:54:08 -0500

    three

---
+++ letsCreateSomeFiles.py
@@ -0,0 +1,16 @@
+#create a few files
+import os
+
+print(os.getcwd())
+fewFile1 = open('American01-23-1945.txt','w')
+fewFile1.write('Hello you ! \n')
+fewFile1.close()
+fewFile2 = open('American02-23-1955.txt','w')
+fewFile2.write('Hello you ! \n')
+fewFile2.close()
+fewFile3 = open('American01-23-1965.txt','w')
+fewFile3.write('Hello you ! \n')
+fewFile3.close()
+fewFile4 = open('American01-23-1975.txt','w')
+fewFile4.write('Hello you ! \n')
+fewFile4.close()
