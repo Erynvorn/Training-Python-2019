52a945898f84f34d8bd1a3e8d9955f85af534b9e
Erynvorn <daniel.seite@gmail.com>
2020-03-01 08:21:46 -0500

    box

---
+++ garageDoor.py
@@ -0,0 +1,6 @@
+podBayDoor = 'open'
+print(podBayDoor)
+assert podBayDoor == 'open' , 'The pod bay doors need to be open "open"'
+podBayDoor = 'I\'m sorry, Dave. I\'m afraid I can\'t do that.'
+print(podBayDoor)
+assert podBayDoor == 'open' , 'The pod bay doors need to be open'
