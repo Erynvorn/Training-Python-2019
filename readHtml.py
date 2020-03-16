f8c84bc832d21c6cc1434b7a9ccdb086a861bca8
Erynvorn <daniel.seite@gmail.com>
2020-03-16 17:00:25 -0400

    map

--- readHtml.py
+++ readHtml.py
@@ -1,10 +1,16 @@
 import requests, bs4
 
-res = requests.get('http://nostarch.com')
-res.raise_for_status()
-noStarchSoup = bs4.BeautifulSoup(res.text)
-print(type(noStarchSoup))
+# res = requests.get('http://nostarch.com')
+# res.raise_for_status()
+# noStarchSoup = bs4.BeautifulSoup(res.text)
+# print(type(noStarchSoup))
 
 exampleFile = open('example.html')
 exampleSoup = bs4.BeautifulSoup(exampleFile)
 print(type(exampleSoup))
+elems = exampleSoup.select('#author')
+print(type(elems))
+print(len(elems))
+print(type(elems[0]))
+auth = elems[0].getText()
+print(auth)
