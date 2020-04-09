318ca067265c708c85b991b576590a54b42489f3
Erynvorn <daniel.seite@gmail.com>
2020-04-09 19:02:45 -0400

    map

---
+++ elitTranslation.py
@@ -0,0 +1,10 @@
+# translate to elite hacker language
+
+def cypher(string):
+    cont = ''
+    translation = { 'I':'1','R':'2','E':'3','A':'4','S':'5','G':'6','T':'7','B':'8','O':'0','l':'1','z':'2','e':'3','a':'4','s':'5','b':'6','t':'7','g':'9','o':'0'}
+    for i in string:
+        cont += translation.get(i, i)
+    return cont
+
+print(cypher('elit'))
