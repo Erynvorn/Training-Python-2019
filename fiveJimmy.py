6f4523a368ae39f4802d34711b8013bd4ce48cd3
Erynvorn <daniel.seite@gmail.com>
2020-04-26 11:05:25 -0400

    weekend

---
+++ fiveJimmy.py
@@ -0,0 +1,20 @@
+print('My name is')
+for i in range(5):
+    print('Jimmy Five Times (' + str(i) + ')')
+
+total = 0
+for num in range(0,51):
+    total = total + num
+print(total)
+print(num)
+print(num*(num+1)/2)
+print('My name is')
+i=0
+while i<9:
+    print('Jimmy Five times (' +str(i) + ')')
+    i= i+1
+
+for i in range(0,10,3):
+    print(i)
+for i in range(10,-1,-2):
+    print(i)
