38c712a9cf66cd57f00e58e4da9018d0712acd97
Erynvorn <daniel.seite@gmail.com>
2020-05-07 07:25:21 -0400

    oupas

---
+++ swapListValue.py
@@ -0,0 +1,14 @@
+def swap_values(a): 
+    b =[]
+    b.append(a[1])
+    b.append(a[0])
+    a.clear()
+    a.append(b[0])
+    a.append(b[1])
+
+
+#swap list values
+
+def swap_values(arr): 
+    arr.reverse()
+    
